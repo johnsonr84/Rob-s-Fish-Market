3f29a6ba653cce9f4bc54f3b94ad31fa0b9df8f6
codedork1 <robertcjohnson1984@gmail.comgit config --global user.emailgit config --global user.email robertcjohnson1984@gmail.com>
2017-05-07 09:40:02 -0600

    chore (event) Adds react events to SotrePicker component

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,13 +2,25 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+  goToStore(event) {
+    event.preventDefault();
+    console.log('You Changed the URL');
+    //first grab the text from the box
+    console.log(this.storeInput.value);
+    //second we're going to transition from / to /store/:storeId
+  }
+
   render() {
+    //Anywhere else
     return (
-      //Anywhere else
-      <form action="" className="store-selector">
+      <form action="" className="store-selector" onSubmit={(e) => this.goToStore(e)}>
       {/* Hello */}
         <h2>Please Enter A Store</h2>
-        <input type="text" require placeholder="Store Name" defaultvalue={getFunName()} />
+        <input type="text" require placeholder="Store Name" defaultvalue={getFunName()} ref={(input) => {this.storeInput = input}} />
         <button type="submit">Visit Store →</button>
       </form>
     )
