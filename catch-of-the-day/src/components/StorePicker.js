d8527363a627c623d7711cf440f79ad2755333a5
codedork1 <robertcjohnson1984@gmail.comgit config --global user.emailgit config --global user.email robertcjohnson1984@gmail.com>
2017-05-07 13:32:59 -0600

    Adds react router to StorePicker

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -10,8 +10,10 @@ class StorePicker extends React.Component {
     event.preventDefault();
     console.log('You Changed the URL');
     //first grab the text from the box
-    console.log(this.storeInput.value);
+    const storeId = this.storeInput.value;
+    console.log(`Going to ${storeId}`)
     //second we're going to transition from / to /store/:storeId
+    this.context.router.transitionTo(`/store/${storeId}`);
   }
 
   render() {
@@ -20,11 +22,15 @@ class StorePicker extends React.Component {
       <form action="" className="store-selector" onSubmit={(e) => this.goToStore(e)}>
       {/* Hello */}
         <h2>Please Enter A Store</h2>
-        <input type="text" require placeholder="Store Name" defaultvalue={getFunName()} ref={(input) => {this.storeInput = input}} />
+        <input type="text" required placeholder="Store Name" defaultvalue={getFunName()} ref={(input) => {this.storeInput = input}} />
         <button type="submit">Visit Store →</button>
       </form>
     )
   }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
