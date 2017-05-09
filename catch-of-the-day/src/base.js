574972288ac9bf01af19d651ee12ab01cf3e808b
codedork1 <robertcjohnson1984@gmail.comgit config --global user.emailgit config --global user.email robertcjohnson1984@gmail.com>
2017-05-08 22:57:29 -0600

    chore (delete) Deletes files and folder to remedy project issues

--- catch-of-the-day/src/base.js
+++
@@ -1,9 +0,0 @@
-import Rebase from 're-base';
-
-const base = Rebase.createClass({
-  apiKey: "AIzaSyBWn6gVJrgsIaxyA8XpsufTZnlLk8f2Opc",
-  authDomain: "robs-fish-market.firebaseapp.com",
-  databaseURL: "https://robs-fish-market.firebaseio.com",
-})
-
-export default base;
